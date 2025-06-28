195b25cb5deba789e70bda30a47b9c759698f6e6
Paulo <pcandradeoliveira@hotmail.com>
2025-06-27 22:54:31 -0300

    Nomenclatura e Convencoes

---
+++ Secao02Fundamentos/07-NomenclaturaConvencoes/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("## Nomenclatura e Convenções ##\n");
+
+//identificadores válidos
+string nome;
+string nomeCompleto;
+int idade;
+int _valo;
+int idade1;
+
+//identificadores inválidos
+//int 5idade;
+//int $valor;
+//int valor#total;
+//string nome completo;
+
+
+
+Console.ReadKey();
