797268225226f1d3e7083a6b407631c978c68c71
Paulo <pcandradeoliveira@hotmail.com>
2025-06-27 22:52:52 -0300

    Nullable Types

---
+++ Secao02Fundamentos/06-NullableTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+//Nullable Types => Tipos anulaveis
+
+int? i = null; //null => valor indefinido ou ausente.
+int b = i ?? 0;
+//aqui estou aviando se (i) for igual a NULL ele será (0).
+//Caso (a) sejá diferente de NULL ele atribui o valor de (a)
+Console.WriteLine(b);
+
+int? idade = null;
+if (idade.HasValue) {
+    Console.WriteLine(idade.Value + " anos");
+} else {
+    Console.WriteLine("Deu certo");
+}
+    Console.ReadKey();
