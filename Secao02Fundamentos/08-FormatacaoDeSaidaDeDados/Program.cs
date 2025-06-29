7e9aed367ea036912a66758b173a62682dbeef9b
Paulo <pcandradeoliveira@hotmail.com>
2025-06-28 22:22:39 -0300

    Ütilizando : Concatenação, Interpolação de string e Places Holders

--- Secao02Fundamentos/08-FormatacaoDeSaidaDeDados/Program.cs
+++ Secao02Fundamentos/08-FormatacaoDeSaidaDeDados/Program.cs
@@ -10,18 +10,17 @@ Console.WriteLine(idade);
 Console.WriteLine("nos \n");
 
 //Concatenação : usando operador de +
-Console.WriteLine("=== Usando Concatenação ===");
+Console.WriteLine("-----Usando Concatenação-----");
 Console.WriteLine(nome + " tem " + idade + " anos\n");
 
 //Interpolação de string : $ -> a interpolação {}
-Console.WriteLine("=== Concatenação de string ===");
+Console.WriteLine("-----Concatenação de string-----");
 Console.WriteLine($"{nome} tem {idade} anos");
 
-
-
-
-
-
+//usando place holder : usa {} com numéração com inicio em zero
+Console.WriteLine("-----place holders-----");
+Console.WriteLine("{0} tem {1} anos ", nome, idade);
+//utilizam a numeracao começando com a chave e dentro o numero que comeca com 0 , apos a vigula colocamos o nome da variavel que queremos que comece.
 
 Console.ReadLine();
 
