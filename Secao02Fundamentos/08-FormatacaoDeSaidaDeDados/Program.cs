9655c28159406abac5af1d87b9ac7061dd8844b9
Paulo <pcandradeoliveira@hotmail.com>
2025-06-28 22:16:04 -0300

    Ütilizando : Concatenação, Interpolação de string e Places Holders

---
+++ Secao02Fundamentos/08-FormatacaoDeSaidaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Saida de dados : Formatação ##\n");
+
+int idade = 20;
+string nome = "Paulo";
+
+//Escrever na mesma linha
+Console.WriteLine(nome);
+Console.WriteLine("tem ");
+Console.WriteLine(idade);
+Console.WriteLine("nos \n");
+
+//Concatenação : usando operador de +
+Console.WriteLine("=== Usando Concatenação ===");
+Console.WriteLine(nome + " tem " + idade + " anos\n");
+
+//Interpolação de string : $ -> a interpolação {}
+Console.WriteLine("=== Concatenação de string ===");
+Console.WriteLine($"{nome} tem {idade} anos");
+
+
+
+
+
+
+
+Console.ReadLine();
+
