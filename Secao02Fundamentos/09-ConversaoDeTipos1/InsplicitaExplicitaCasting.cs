1047a0169cdcf36dce039dbd5586af35415cc7db
Paulo <pcandradeoliveira@hotmail.com>
2025-06-29 00:34:58 -0300

    Tipo Inplícito e Explicito e utilizando Casting

---
+++ Secao02Fundamentos/09-ConversaoDeTipos1/InsplicitaExplicitaCasting.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Conversão de Tipos ##\n");
+
+// Conversão Implícita
+// Podemos fazer essa conversão porque os dois tipos são compatíveis.
+// int ocupa 4 bytes e double ocupa 8 bytes.
+// Ou seja, é possível converter de int para double sem perda de dados.
+int varInt = 100;
+double varDouble = varInt;
+Console.WriteLine(varDouble);
+
+Console.WriteLine("\nConversão Explícita");
+//double usa 8 bits enquanto Int usa 4 bits com isso não conseguimos converter.
+//Para convertermos devemos fazer o uso do Casting
+//adicionando dentro do ( ) o tipo de dado para o que queremos que seja convertido.
+double varDoublee = 12.456;
+int varIntt = (int)varDoublee;
+Console.WriteLine(varIntt);
+
+
+
+Console.ReadLine();
+
+
+
